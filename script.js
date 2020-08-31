c635011e2bee6d0756af57b55d3aa9fe9adc6ca1
Conanas <tmhaigh@gmail.com>
2020-08-31 11:27:38 +1000

    update to rep tracker

--- script.js
+++ script.js
@@ -1,28 +1,46 @@
 var repTotal = document.getElementById("repTotal");
 var restBetweenReps = document.getElementById("restBetweenReps");
+var repsDisplay = document.getElementById("repsDisplay");
 var timeDisplay = document.getElementById("timeDisplay");
 var startTimerButton = document.getElementById("startTimer");
 var interval;
-var timeRemaining = 0;
+var timeElapsed = 0;
+var repsElapsed = 0;
 
 startTimerButton.addEventListener("click", startTimer);
 
 function startTimer() {
     updateTimeDisplay();
+    updateReps();
+    updateRepsDisplay();
     interval = setInterval(function() {
-        timeRemaining++;
+        timeElapsed++;
         updateTimeDisplay();
-        if (timeRemaining == restBetweenReps.value) {
+        if (timeElapsed >= restBetweenReps.value) {
             stopTimer();
+            updateReps();
+            updateRepsDisplay();
         }
     }, 1000);
 }
 
 function updateTimeDisplay() {
-    timeDisplay.textContent = `${restBetweenReps.value - timeRemaining}`;
+    timeDisplay.textContent = `${restBetweenReps.value - timeElapsed}`;
 }
 
 function stopTimer() {
     clearInterval(interval);
-    timeRemaining = 0;
+    timeElapsed = 0;
+}
+
+function updateReps() {
+    if (repsElapsed == repTotal.value) {
+        repsElapsed = 0;
+    } else {
+        repsElapsed++;
+    }
+}
+
+function updateRepsDisplay() {
+    repsDisplay.textContent = `${repTotal.value - repsElapsed}`
 }
