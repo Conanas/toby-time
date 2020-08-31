3799a995fa1dc550dfcd605ac7ba220dcee7f7d9
Conanas <tmhaigh@gmail.com>
2020-08-31 10:51:15 +1000

    declaring variables

---
+++ script.js
@@ -0,0 +1,4 @@
+var repTotal = document.getElementById("repTotal");
+var restBetweenReps = document.getElementById("restBetweenReps");
+var timeDisplay = document.getElementById("timeDisplay");
+var startTimerButton = document.getElementById("startTimer");
