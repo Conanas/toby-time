a4842bd743037642f3b8702ad6e1c6d5e368478f
Conanas <tmhaigh@gmail.com>
2020-08-31 11:13:00 +1000

    timer counts down successfully

--- script.js
+++ script.js
@@ -2,3 +2,27 @@ var repTotal = document.getElementById("repTotal");
 var restBetweenReps = document.getElementById("restBetweenReps");
 var timeDisplay = document.getElementById("timeDisplay");
 var startTimerButton = document.getElementById("startTimer");
+var interval;
+var timeRemaining = 0;
+
+startTimerButton.addEventListener("click", startTimer);
+
+function startTimer() {
+    updateTimeDisplay();
+    interval = setInterval(function() {
+        timeRemaining++;
+        updateTimeDisplay();
+        if (timeRemaining == restBetweenReps.value) {
+            stopTimer();
+        }
+    }, 1000);
+}
+
+function updateTimeDisplay() {
+    timeDisplay.textContent = `${restBetweenReps.value - timeRemaining}`;
+}
+
+function stopTimer() {
+    clearInterval(interval);
+    timeRemaining = 0;
+}
